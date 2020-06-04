af3848c51e95598e20fd5269fc5294aa56192d89
kim jongkyu <jong9181@gmail.com>
2020-06-04 23:21:56 +0900

    Weather  + 200604
    날씨 세부사항 추가

--- src/PublicDateGetter.java
+++ src/PublicDateGetter.java
@@ -4,6 +4,11 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.Scanner;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 import java.io.IOException;
 
 public class PublicDateGetter {
@@ -171,6 +176,53 @@ public class PublicDateGetter {
 		return val;
 	}
 	
+	String weather() {
+		try{
+            //서울시청의 위도와 경도
+            String lon = "127.004681";  //경도
+            String lat = "37.276848";   //위도
+
+            //OpenAPI call하는 URL
+            String urlstr = "http://api.openweathermap.org/data/2.5/weather?"
+                        + "lat="+lat+"&lon="+lon
+                        +"&appid=082b31d2f7ab5b654775052d887b7b0c";
+            URL url = new URL(urlstr);
+            BufferedReader bf;
+            String line;
+            String result="";
+
+            //날씨 정보를 받아온다.
+            bf = new BufferedReader(new InputStreamReader(url.openStream()));
+
+            //버퍼에 있는 정보를 문자열로 변환.
+            while((line=bf.readLine())!=null){
+                result=result.concat(line);
+                //System.out.println(line);
+            }
+
+            //문자열을 JSON으로 파싱
+            JSONParser jsonParser = new JSONParser();
+            JSONObject jsonObj = (JSONObject) jsonParser.parse(result);
+
+            //지역 출력
+            System.out.println("지역 : " + jsonObj.get("name"));
+
+            //날씨 출력	
+            JSONArray weatherArray = (JSONArray) jsonObj.get("weather");
+            JSONObject obj = (JSONObject) weatherArray.get(0);
+            System.out.println("날씨 : "+obj.get("main"));
+
+            //온도 출력(절대온도라서 변환 필요)
+            JSONObject mainArray = (JSONObject) jsonObj.get("main");
+            double ktemp = Double.parseDouble(mainArray.get("temp").toString());
+            double temp = ktemp-273.15;
+            System.out.printf("온도 : %.2f\n",temp);
+
+            bf.close();
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+	}
 	
 	
 }
